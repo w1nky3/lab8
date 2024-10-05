5bb97789ba714d4c03074c99d25a3357ce94a3d6
Владимир Экизян <9885313888va@gmail.com>
2024-10-05 06:30:13 +0300

    Initial commit

---
+++ time.pas
@@ -0,0 +1,9 @@
+﻿begin
+  var year:= ReadInteger('Введите год:');
+  if (year mod 100 = 0) and (year mod 400 <> 0) then
+    Print('Год не високосный')
+  else if year mod 4 = 0 then
+    Print('Год високосный')
+  else
+    Print('Год не вискокосный')
+end.
